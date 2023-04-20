2dd6fa0bff8be5717278b2db505501439fc91ac9
Genisc13 <genis.castillo@estudiantat.upc.edu>
2023-04-20 13:15:08 +0200

    completado minimo1

--- src/app/components/notification-components/notification-details/notification-details.component.ts
+++ src/app/components/notification-components/notification-details/notification-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-notification-details',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification-details.component.css']
 })
 export class NotificationDetailsComponent {
+  avisoData: any;
+  notificationId!: string;
+  isModalOpen:boolean=false;
+  
+  constructor(private route: ActivatedRoute, private notificationService: NotificationService,private router:Router) {}
 
+  ngOnInit(): void {
+    this.loadUserData();
+  }
+
+  loadUserData(): void {
+    const url = this.route.snapshot.url.join('/');
+    const parts = url.split('/');
+    this.notificationId = parts[parts.length - 1];
+    console.log(this.notificationId);
+    this.notificationService.getNotification(this.notificationId).subscribe(avisoData=>{
+      this.avisoData=avisoData;
+    });
+  }
 }
